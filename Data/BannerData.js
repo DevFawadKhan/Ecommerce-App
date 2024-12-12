6226ffa1769aaabb60934963e1313ec2a78ea083
DevFawadKhan <mfawadkhan05@gmail.com>
2024-12-12 08:45:53 -0700

    working on product detail module

--- Data/BannerData.js
+++ Data/BannerData.js
@@ -1,9 +1,11 @@
 export const data = [
     {
       _id:1,
-      title: "Aenean leo",
-      body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
-      imgUrl: "https://picsum.photos/id/11/200/300",
+      title: "Apple - 10.9-Inch iPad - Latest Model - (10th Generation) with Wi-Fi - 64GB - Blue",
+      body: "Easy set up, excellent quality.  Purchased to replace a 12-year old model that become obsolete, and am very pleased with the speed and ease of use",
+      price:"1000$",
+       quantity:10,
+      imgUrl: "https://pisces.bbystatic.com/image2/BestBuy_US/dam/REF-1652362-cym-iphone16plus_DER-b4ab2899-2244-42a3-a961-a4475f4e7dec.jpg"
     },
     // {
     //   _id:2,
--- screens/Productdetails.js
+++ screens/Productdetails.js
@@ -1,11 +1,78 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const Productdetails = () => {
+import { View, Text,Image,StyleSheet,Dimensions, TouchableOpacity } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import {data} from '../../client/Data/BannerData.js'
+const Productdetails = ({route}) => {
+  // id store in params
+  const {params}=route
+const [product, setproduct] = useState({})
+useEffect(()=>{
+const getproduct=data.find((p)=>{
+  return p?._id===params?._id;
+})
+setproduct(getproduct);
+},[params._id])
   return (
     <View>
-      <Text>Productdetails</Text>
+        <Image style={style.image} source={{uri:product?.imgUrl}}></Image>
+        <View style={style.container}>
+        <Text style={style.title}>{product.title}</Text>
+        <Text style={style.price}>Price:{product.price}</Text> 
+        <Text style={style.desc} >{product.body}</Text>         
+        </View>
+        {/* add to cart */}
+        <View style={style.cart}>
+          <TouchableOpacity style={style.cartbtn}>
+            <Text style={style.btntext}>ADD TO CART</Text>
+          </TouchableOpacity>
+          <View><Text>Hi</Text></View>
+        </View>
     </View>
   )
 }
 export default Productdetails
+const { width } = Dimensions.get('window'); // Get screen width
+const style=StyleSheet.create({
+  image:{
+    width: width-20, 
+    height:400,
+    borderRadius:10,
+    marginHorizontal:13,
+  },
+  container:{
+marginVertical:15,
+marginHorizontal:15,
+  },
+  title:{
+fontSize:15,
+textAlign:'left'
+  },
+  price:{
+fontWeight:'bold',
+  },
+  desc:{
+fontSize:12,
+textTransform:'capitalize',
+marginVertical:10,
+textAlign:'justify'
+  },
+  cart:{
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+    marginVertical:20,
+    marginHorizontal:20
+  },
+  cartbtn:{
+    width:180,
+    backgroundColor:'black',
+    borderRadius:10,
+height:40,
+justifyContent:'center'
+  },
+  btntext:{
+    color:'white',
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize:15
+  }
+})
